8a97a65b86e10c7a9e7c4ec75c3eb743564d2dad
ciit-kojiescolar <129504627+ciit-kojiescolar@users.noreply.github.com>
2024-07-08 20:15:06 +0800

    Updated and Added new Features
    Ready to pass

--- src/firebase/config.js
+++ src/firebase/config.js
@@ -1,16 +1,20 @@
-import {initializeApp} from 'firebase/app'
-import {getFirestore} from 'firebase/firestore'
+import {initializeApp} from 'firebase/app';
+import {getFirestore} from 'firebase/firestore';
+import {getAuth} from "firebase/auth";
 
 const firebaseConfig = {
-    apiKey: "AIzaSyCRwWd1E7JJiSuHK-J3FRXj8wjK3-WO84E",
-    authDomain: "reactdemojuly2024.firebaseapp.com",
-    projectId: "reactdemojuly2024",
-    storageBucket: "reactdemojuly2024.appspot.com",
-    messagingSenderId: "140474381397",
-    appId: "1:140474381397:web:314537f93c0cd60f1090dc"
+  apiKey: "AIzaSyDM2cogZWDuLkk2-dQ0X5o7NncxJOgMNfQ",
+  authDomain: "fir-project-3d679.firebaseapp.com",
+  projectId: "fir-project-3d679",
+  storageBucket: "fir-project-3d679.appspot.com",
+  messagingSenderId: "655122527695",
+  appId: "1:655122527695:web:2a66d064c3ce93d035ceba",
+  //measurementId: "G-GHFV92Z8B9"
 };
 
   initializeApp(firebaseConfig);
+
+  export const auth=getAuth();
 
   const db = getFirestore();
 
---
+++ src/pages/UpdateArticle.js
@@ -0,0 +1,76 @@
+import { useState, useEffect } from 'react'
+import { useNavigate, useParams } from 'react-router-dom'
+import { doc, getDoc, updateDoc } from 'firebase/firestore'
+import { db } from '../firebase/config'
+// styles
+import './create.css'
+
+export default function UpdateArticle() {
+  const { id } = useParams()
+  const [title, setTitle] = useState('')
+  const [author, setAuthor] = useState('')
+  const [description, setDescription] = useState('')
+
+  const navigate = useNavigate()
+
+  useEffect(() => {
+    const fetchArticle = async () => {
+      const ref = doc(db, 'articles', id)
+      const docSnap = await getDoc(ref)
+      if (docSnap.exists()) {
+        const data = docSnap.data()
+        setTitle(data.title)
+        setAuthor(data.author)
+        setDescription(data.description)
+      }
+    }
+    fetchArticle()
+  }, [id])
+
+  const handleSubmit = async (e) => {
+    e.preventDefault()
+    const article = { title, author, description }
+    const ref = doc(db, 'articles', id)
+    await updateDoc(ref, article)
+    navigate('/')
+  }
+
+  return (
+    <div className="create">
+      <h2 className="page-title">Update Article</h2>
+      <form onSubmit={handleSubmit}>
+
+        <label>
+          <span>Title:</span>
+          <input
+            type="text"
+            onChange={(e) => setTitle(e.target.value)}
+            value={title}
+            required
+          />
+        </label>
+
+        <label>
+          <span>Author:</span>
+          <input
+            type="text"
+            onChange={(e) => setAuthor(e.target.value)}
+            value={author}
+            required
+          />
+        </label>
+
+        <label>
+          <span>Description:</span>
+          <textarea
+            onChange={(e) => setDescription(e.target.value)}
+            value={description}
+            required
+          />
+        </label>
+
+        <button className="btn">Save</button>
+      </form>
+    </div>
+  )
+}
